d15cd31f018a638d6cf603c9aa319d7cfa58478d
FotisVach <fotisvach4@gmail.com>
2021-12-08 01:16:34 +0200

    Add quicktest

---
+++ src/test/java/fotis/designPatterns/quicktest/Quicktest.java
@@ -0,0 +1,22 @@
+package fotis.designPatterns.quicktest;
+
+import static org.junit.jupiter.api.Assertions.fail;
+
+import org.junit.Ignore;
+import org.junit.jupiter.api.Test;
+
+/**
+ * Quicktest 
+ */
+@Ignore
+class Quicktest {
+
+	/**
+	 * Quicktest method
+	 */
+	@Test
+	void test() {
+		fail("Not yet implemented"); //$NON-NLS-1$
+	}
+
+}
